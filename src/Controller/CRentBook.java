a7198fa9fffeda437ba0e7d3861370064653d2fa
aboedhiprasojo <46617242+aboedhiprasojo@users.noreply.github.com>
2021-07-12 17:34:34 +0700

    Commit Perpustakaan

---
+++ src/Controller/CRentBook.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.DAOSewaBuku;
+import IDAO.IDAOSewaBuku;
+import Model.TabelSewaBuku;
+import Model.mTable;
+import View.RentBook;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CRentBook {
+    public cRentBook(maingui _tampil){
+        this._tampilan = _tampil;
+        iDAOSewaBuku = new DAOSewaBuku();
+    }
+    
+        public void IsiTable(){
+      list_sewaBuku = iDAOSewaBuku.GetAll();
+      TabelSewaBuku tabelSewaBuku = new TabelSewaBuku(list_sewaBuku);
+      _tampilan.GetTabelData().setModel(tabelSewaBuku);
+        
+    }
+       
+       maingui _tampilan;
+       IDAOSewaBuku iDAOSewaBuku;
+       List<mTable> list_sewaBuku;
+}
